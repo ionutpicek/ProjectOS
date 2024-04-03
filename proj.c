2d3b60166937a2b1b1e3c4a68995e784f1f04ab9
ionutpicek <153368553+ionutpicek@users.noreply.github.com>
2024-04-03 14:31:52 +0300

    Task -> README

--- proj.c
+++ proj.c
@@ -1,10 +1,3 @@
-//S1
-//Description: The proposed project combines functionalities for monitoring a directory to manage differences between two captures (snapshots) of it. 
-//The user will be able to observe and intervene in the changes in the monitored directory.
-//Directory Monitoring:
-//The user can specify the directory to be monitored as an argument in the command line, and the program will track changes occurring in it and its subdirectories, 
-//parsing recursively each entry from the directory.
-//With each run of the program, the snapshot of the directory will be updated, storing the metadata of each entry.
 
 #include <stdio.h>
 #include <stdlib.h>
